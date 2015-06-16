2728b59c203fe0c86b02e26be74befa59973b43d
Aziz Kapadia <aziz@azizweb.com>
2015-06-16 20:47:22 +0100

    Web Client and Core Projects are added

---
+++ source/Lotus.WebClient/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace Lotus.WebClient.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
