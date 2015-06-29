dd6ecce98003a494689b5393776008b620b3293f
lotkeamol <amollotke@yahoo.co.in>
2015-06-29 22:48:42 +0100

    Database context added

---
+++ source/Innoventory.Lotus.Domain/DataEntities/InnoventoryDBContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Innoventory.Lotus.Domain.DataEntities
+{
+    public class InnoventoryDBContext : DbContext
+    {
+        public InnoventoryDBContext()
+            : base("name=InnoventoryDBcontext")
+        {
+
+        }
+
+       
+
+        public DbSet<Product> Product { get; set; }
+        public DbSet<ProductVariant> ProductVariant { get; set; }
+        
+
+        
+        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        //{
+        ////For Table Mapping 
+            
+        //    modelBuilder.Entity<Budget>().ToTable("tblBudget");
+
+        ////For Preventing Table Creation
+        //    modelBuilder.Ignore<UserAccount>();
+
+
+        //    //For multiple keys
+        //    modelBuilder.Entity<VoucherDetail>().HasKey(x => new { x.VoucherHeaderID, x.VoucherDetailID });
+            
+        //    base.OnModelCreating(modelBuilder);
+
+
+        //}
+    }
+}
