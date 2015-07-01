9312ea0244cada0ea430785ca5621b5c9600e945
Aziz Kapadia <aziz@azizweb.com>
2015-07-01 19:39:58 +0100

    Missing commits

---
+++ source/Innoventory.Api/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Innoventory.Api.Controllers
+{
+    [RoutePrefix("../api/v1/")]
+    public class HomeController : ApiController
+    {
+
+        public IHttpActionResult Get()
+        {
+            return Ok("Success");
+        }
+    }
+}
---
+++ source/Innoventory.Lotus.WebClient/custom-scripts/innoventoryApp.js
@@ -0,0 +1,18 @@
+﻿(function () {
+
+    var underscore = angular.module('underscore', []);
+    underscore.factory('_', function () {
+        return window._;
+    });
+
+    var innoventoryApp = angular.module("innoventoryApp", ["_", "product-directives"]);
+
+    innoventoryApp.controller("productController", productController);
+
+    innoventoryApp.factory("productService", productService);
+
+    innoventoryApp.factory("innoventoryService", innoventoryService);
+
+   
+
+})()
