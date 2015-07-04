990585ce64cdf7ba0b8455b3503db2f9dd83e141
Aziz Kapadia <aziz@azizweb.com>
2015-07-04 10:33:21 +0100

    Missing checkins

---
+++ source/Innoventory.Lotus.Bootstraper/DependencyContainer.cs
@@ -0,0 +1,24 @@
+﻿using Innoventory.Lotus.DataAccess.Concrete;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition.Hosting;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Innoventory.Lotus.Core
+{
+    public class DependencyContainer
+    {
+        public static CompositionContainer Initialize()
+        {
+            AggregateCatalog catalog = new AggregateCatalog();
+
+            catalog.Catalogs.Add(new AssemblyCatalog(typeof(AddressRepository).Assembly));
+
+            CompositionContainer container = new CompositionContainer(catalog);
+
+            return container;
+        }
+    }
+}
