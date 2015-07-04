842610ba8480d470a85ab35960d4a0a1e5cf42ca
Aziz Kapadia <aziz@azizweb.com>
2015-07-04 14:41:16 +0100

    Client side project changes

---
+++ source/Innoventory.Lotus.WebClient/custom-scripts/shared/innoventory.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    var global = function () {
+        var globals = {
+
+            webApiBaseUrl: "http://localhost/api/v1/"
+
+        };
+
+        return globals;
+    };
+})(window.Innoventory);
