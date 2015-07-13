3893972739425a38c6d7ead923c6171174bbe0e0
lotkeamol <amollotke@yahoo.co.in>
2015-07-13 11:47:31 +0100

    Product ViewModel

--- source/Innoventory.Lotus.ViewModels/ProductViewModel.cs
+++ source/Innoventory.Lotus.ViewModels/ProductViewModel.cs
@@ -17,60 +17,75 @@ namespace Innoventory.Lotus.ViewModels
         [DataMember]
         public Guid ProductId { get; set; }
 
-
+        [DisplayName("Item Type")]
         [DataMember]
         public int ItemType { get; set; }
 
+        [DisplayName("Item Type Value")]
         [DataMember]
         public string ItemTypeValue { get; set; }
 
+        [DisplayName("Product Name")]
         [DataMember]
         public string ProductName { get; set; }
 
+        [DisplayName("Description")]
         [DataMember]
         public string Description { get; set; }
 
+        [DisplayName("Remarks")]
         [DataMember]
         public string Remarks { get; set; }
 
+        [DisplayName("Category ID")]
         [DataMember]
         public Guid CategoryId { get; set; }
 
+        [DisplayName("Category Name")]
         [DataMember]
         public string CategoryName { get; set; }
 
+        [DisplayName("Sub Category ID")]
         [DataMember]
         public Guid SubCategoryId { get; set; }
 
+        [DisplayName("Sub Category Name")]
         [DataMember]
-
         public string SubCategoryName { get; set; }
 
-
+        [DisplayName("Re-Order Point")]
         [DataMember]
         public decimal? ReorderPoint { get; set; }
 
+        [DisplayName("Re-Order Quantity")]
         [DataMember]
         public decimal? ReorderQuantity { get; set; }
 
+        [DisplayName("Unit ID")]
         [DataMember]
         public Guid UnitId { get; set; }
 
+        [DisplayName("Unit")]
         [DataMember]
         public string Unit { get; set; }
 
+        [DisplayName("Image ID")]
         [DataMember]
         public Guid? ImageId { get; set; }
 
+        [DisplayName("Sales Order Unit ID")]
         [DataMember]
         public Guid SalesOrderUnitId { get; set; }
 
+        [DisplayName("Sales Order Unit")]
         [DataMember]
         public string SalesOrderUnit { get; set; }
 
+        [DisplayName("Purchase Order Unit ID")]
         [DataMember]
         public Guid PurchaseOrderUnitId { get; set; }
 
+        [DisplayName("Purschase Order Unit")]
         [DataMember]
         public Guid PurchaseOrderUnitName { get; set; }
 
