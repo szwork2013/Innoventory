7e46e7ae2a8a5a412e8f03fe65e207d298b173ba
lotkeamol <amollotke@yahoo.co.in>
2015-07-13 12:00:25 +0100

    User Role ViewModel

--- source/Innoventory.Lotus.ViewModels/UserRoleViewModel.cs
+++ source/Innoventory.Lotus.ViewModels/UserRoleViewModel.cs
@@ -1,24 +1,41 @@
-﻿using System;
+﻿using Innoventory.Lotus.Core.Contracts;
+using Innoventory.Lotus.Database.DataEntities;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
-using Innoventory.Lotus.Core.Contracts;
 
 namespace Innoventory.Lotus.ViewModels
 {
+    [DataContract]
     public class UserRoleViewModel:IIdentifiable
     {
-
+        [ScaffoldColumn(false)]
+        [DataMember]
         public Guid UserRoleId { get; set; }
 
+        [DisplayName("Role Name")]
+        [DataMember]
         public string RoleName { get; set; }
 
+        [DisplayName("Role Description")]
+        [DataMember]
         public string RoleDescription { get; set; }
+        
+        [DataMember]
         public DateTime CreatedDate { get; set; }
+
+        [DataMember]
         public Guid CreatedBy { get; set; }
+        
+        [DataMember]
         public DateTime ModifiedDate { get; set; }
+        
+        [DataMember]
         public Guid ModifiedBy { get; set; }
 
         public List<UserAccountUserRoleMapViewModel> UserAccountRoleMaps { get; set; }
@@ -28,11 +45,11 @@ namespace Innoventory.Lotus.ViewModels
         {
             get
             {
-                throw new NotImplementedException();
+                return UserRoleId;
             }
             set
             {
-                throw new NotImplementedException();
+                UserRoleId = value;
             }
         }
     }
